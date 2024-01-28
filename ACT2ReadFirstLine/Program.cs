30b61f10d17e99fe0cf39db9975e98bcd9ebe231
Marcos <marcos.gorriaran.7e7@itb.cat>
2024-01-28 21:44:26 +0100

    Finished Activity 2

---
+++ ACT2ReadFirstLine/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ACT2
+{
+    public class Execute
+    {
+        public static void Main()
+        {
+            const string AskFileDirectory = "Proporcioname la direccion del archivo que desees que acceda: ";
+            const string FileNotExistsError = "El archivo especificado no existe";
+            const string DirectoryNotExistsError = "La ruta especificada no existe";
+            string fileName;
+            try
+            {
+                Console.Write(AskFileDirectory);
+                fileName = Console.ReadLine()??"";
+
+                Console.Write(ReadFirstLine(fileName));
+            }catch (FileNotFoundException)
+            {
+                Console.WriteLine(FileNotExistsError);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine(DirectoryNotExistsError);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+            }
+        }
+        public static string ReadFirstLine(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            return sr.ReadLine()??"";
+        }
+    }
+}
